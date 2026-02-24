0e01c3769d8c9c023f65465de1a53652c6fd8bfd
庭师 <593541465@qq.com>
2026-02-24 10:25:47 +0800

    1

--- Webrtc_NoisyReduce/ENC/channel_buffer.c
+++
@@ -1,6 +0,0 @@
-#include "channel_buffer.h"
-
-#include <cstdint>
-
-#include "audio_util.h"
-#include "base/checks.h"
--- Webrtc_NoisyReduce/ENC/stream_config.h
+++
@@ -1,6 +0,0 @@
-#ifndef MODULES_AUDIO_PROCESSING_AUDIO_BUFFER_H_
-#define MODULES_AUDIO_PROCESSING_AUDIO_BUFFER_H_
-
-#define kChunkSizeMs 10
-#define AudioProcessing_GetFrameSize(sample_rate_hz) (sample_rate_hz / 100)
-
